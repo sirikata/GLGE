6e977b0b9746b4ffab29990a156dbe4432ed8ba5
danx0r <danbmil99@gmail.com>
2010-11-03 13:57:25 -0700

    rm spurious log

--- glge.js
+++ glge.js
@@ -5661,7 +5661,6 @@ GLGE.Renderer=function(canvas,error){
 		this.gl = canvas.getContext("experimental-webgl",{alpha:true,depth:true,stencil:true,antialias:true,premultipliedAlpha:true});
 	} catch(e) {}
 	if(!this.gl) {
-        console.log("GLGE err:", typeof(globalNoWebGLError)=="undefined")
 		if( (!error) && (typeof(globalNoWebGLError)=="undefined")){
 			var div=document.createElement("div");
 			div.setAttribute("style","position: absolute; top: 10px; left: 10px; font-family: sans-serif; font-size: 14px; padding: 10px;background-color: #fcffcb;color: #800; width: 200px; border:2px solid #f00");
